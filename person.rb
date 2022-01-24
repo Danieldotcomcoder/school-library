a87426c4cd6f90dd4db920a8242c88c34f438583
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2022-01-24 15:58:49 +0200

    Fix: Issues

--- person.rb
+++ person.rb
@@ -3,7 +3,7 @@ class Person
   attr_accessor :name, :age
 
   def initialize(age, name = 'Unknown', parent_permission: true)
-    @id = id
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
@@ -12,7 +12,7 @@ class Person
   private :of_age?
 
   def of_age?
-    return unless @age >= 18
+    @age >= 18
   end
 
   def can_use_services?
