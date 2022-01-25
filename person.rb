24cc3809cd45e15defddcb1199d04c0f9bfcf4ad
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2022-01-25 10:31:30 +0200

    Added: corrector class

--- person.rb
+++ person.rb
@@ -1,3 +1,5 @@
+require './corrector'
+
 class Person
   attr_reader :id
   attr_accessor :name, :age
@@ -7,6 +9,7 @@ class Person
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @corrector = Corrector.new
   end
 
   private :of_age?
@@ -17,5 +20,9 @@ class Person
 
   def can_use_services?
     of_age? || parent_permission
+  end
+
+  def validate_name
+   @name = @corrector.correct_name(name)
   end
 end
